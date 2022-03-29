d9716662353434d8684d6be00149bb3685ddc69b
safwansadi <safwaniftekher@gmail.com>
2022-03-29 23:44:36 +0600

    simple pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+//app
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+  console.log(posts);
+
+  const handleNextButton = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handleBackButton = () => {
+    setCurrentPage(currentPage - 1);
+  };
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>See Posts</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      {currentPosts.length == postsPerPage ? (
+        <div>
+          <button onClick={() => handleBackButton()}>back</button>
+          <Pagination
+            postsPerPage={postsPerPage}
+            totalPosts={posts.length}
+            paginate={paginate}
+          />
+          <button onClick={() => handleNextButton()}>Next</button>
+        </div>
+      ) : (
+        <button onClick={() => handleBackButton()}>back</button>
+      )}
     </div>
   );
 }
